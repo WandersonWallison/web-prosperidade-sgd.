ab26636c61d71d208c7f0670ff6bcbf310af46ec
Wanderson Silva <wanderson.silva@atpse.ad.corp>
2019-05-10 20:54:26 -0300

    novos campos update

--- src/components/Dashboard/Views/Forms/FormUserEdit.vue
+++ src/components/Dashboard/Views/Forms/FormUserEdit.vue
@@ -64,11 +64,13 @@
             </fg-input>
           </div>
           <div class="col-md-4">
-            <fg-input type="text"
-                      label="Estado"
-                      v-model="user.endereco[0].uf">
-            </fg-input>
-          </div>
+                    <fg-input>
+                        <el-select class="select-default" v-model="user.endereco[0].uf" name="tipo_user"  placeholder="Selecione...">
+                            <el-option class="select-default" v-for="item in optionsStade" :key="item.value" :label="item.label" :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </fg-input>
+                </div>
         </div>
 
         <div class="row">
@@ -104,17 +106,170 @@
   
 </template>
 <script>
+import axios from 'axios'
+import {Select, Option} from 'element-ui'
+import swal from 'sweetalert2'
   export default {
       name: 'UserEdit',
       props: ['selected'],
+      components: {
+      [Select.name]: Select,
+      [Option.name]: Option
+    },
     data () {
       return {
-        user: this.selected
+        user: this.selected,
+        optionsStade: [{
+                    value: 'AC',
+                    label: 'Acre'
+                },
+                {
+                    value: 'AL',
+                    label: 'Alagoas'
+                },
+                {
+                    value: 'AP',
+                    label: 'Amapá'
+                },
+                {
+                    value: 'AM',
+                    label: 'Amazonas'
+                },
+                {
+                    value: 'BA',
+                    label: 'Bahia'
+                },
+                {
+                    value: 'CE',
+                    label: 'Cear?'
+                },
+                {
+                    value: 'DF',
+                    label: 'Distrito Federal'
+                },
+                {
+                    value: 'ES',
+                    label: 'Espírito Santo'
+                },
+                {
+                    value: 'GO',
+                    label: 'Goiás'
+                },
+                {
+                    value: 'MA',
+                    label: 'Maranhão'
+                },
+                {
+                    value: 'MT',
+                    label: 'Mato Grosso'
+                },
+                {
+                    value: 'MS',
+                    label: 'Mato Grosso do Sul'
+                },
+                {
+                    value: 'MG',
+                    label: 'Minas Gerais'
+                },
+                {
+                    value: 'PA',
+                    label: 'Par?'
+                },
+                {
+                    value: 'PB',
+                    label: 'Paraiba'
+                },
+                {
+                    value: 'PR',
+                    label: 'Paraná'
+                },
+                {
+                    value: 'PE',
+                    label: 'Pernambuco'
+                },
+                {
+                    value: 'PI',
+                    label: 'Piauí'
+                },
+                {
+                    value: 'RJ',
+                    label: 'Rio de Janeiro'
+                },
+                {
+                    value: 'RN',
+                    label: 'Rio Grande do Norte'
+                },
+                {
+                    value: 'RS',
+                    label: 'Rio Grande do Sul'
+                },
+                {
+                    value: 'RO',
+                    label: 'Rondônia'
+                },
+                {
+                    value: 'RR',
+                    label: 'Roraima'
+                },
+                {
+                    value: 'SC',
+                    label: 'Santa Catarina'
+                },
+                {
+                    value: 'SP',
+                    label: 'S?o Paulo'
+                },
+                {
+                    value: 'SE',
+                    label: 'Sergipe'
+                },
+                {
+                    value: 'TO',
+                    label: 'Tocantins'
+                }
+            ]
       }
     },
     methods: {
       updateProfile () {
-        alert('Your data: ' + JSON.stringify(this.user))
+        let userEdit= {
+                username: this.user.firstName,
+                email: this.user.email,
+                password: this.user.senha,
+                cpf: this.user.cpf,
+                cnh: this.user.cnh,
+                rg: this.user.rg,
+                data_nascimento: this.user.datePicker,
+                telefone: this.user.telefone,
+                celular: this.user.celular,
+                id_grupo: this.user.tipo
+                }
+        let endereco = {
+                logradouro: this.user.logradouro,
+                cep: this.user.cep,
+                bairro: this.user.bairro,
+                cidade: this.user.cidade,
+                uf: this.user.estado,
+                numero: this.user.numero,
+                // id_user: ''
+      }
+                axios.put(process.env.VUE_APP_ROOT_API  + '/user/'+ this.user.id, userEdit)
+                .then(response => {
+                    this.results = response.data
+                    // endereco.id_user = response.data.id
+                      axios.put(process.env.VUE_APP_ROOT_API + '/endereco/'+ this.user.endereco[0].id, endereco)
+                        .then(response => {
+                    swal('Bom trabalho!', 'Usuario Cadastrado com sucesso!', 'success')
+                    window.location.reload()
+                    // this.$router.push('/forms/UserList')
+                    })
+                  })
+                    .catch(error => {
+                        // alert(error.response.data)
+                        swal('Algo de errado!', 'Email invalido!', 'error')
+                        console.log(error.response.data)
+                        })
+        // alert('Your data: ' + JSON.stringify(this.user))
       }
     }
   }
