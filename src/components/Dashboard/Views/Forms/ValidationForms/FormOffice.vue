1f6fd9d001074b797e8b92b1523d0e47e8375fa8
Wellington Santos <wellington.m.santos@gmail.com>
2019-05-10 17:20:57 -0300

    Alterações do Escritorio e Empresa

--- src/components/Dashboard/Views/Forms/ValidationForms/FormOffice.vue
+++ src/components/Dashboard/Views/Forms/ValidationForms/FormOffice.vue
@@ -1,151 +1,308 @@
 <template>
-  <div class="card">
+<div class="card">
     <form>
-      <div class="card-header">
-        <h4 class="card-title">
-          Cadastro de Escritório
-        </h4>
-      </div>
-      <div class="card-body">
-        <div class="form-group">
-          <label>Nome</label>
-          <fg-input  type="text"
-                     name="nome"
-                     v-validate="modelValidations.nome"
-                     :error="getError('nome')"
-                     v-model="model.nome">
-          </fg-input>
-          <label>CNPJ</label>
-          <fg-input  type="text"
-                     name="cnpj"
-                     v-validate="modelValidations.cnpj"
-                     :error="getError('cnpj')"
-                     v-model="model.cnpj">
-          </fg-input>
-          <label>Telefone</label>
-          <fg-input  type="text"
-                     name="telefone"
-                     v-validate="modelValidations.telefone"
-                     :error="getError('telefone')"
-                     v-model="model.telefone">
-          </fg-input>
-          <label>E-mail</label>
-          <fg-input  type="email"
-                     name="email"
-                     v-validate="modelValidations.email"
-                     :error="getError('email')"
-                     v-model="model.email">
-          </fg-input>
+        <div class="card-header">
+            <h4 class="card-title">
+                Cadastro de EscritÃ³rio
+            </h4>
         </div>
-        <div class="form-group">
-          <label>Logradouro</label>
-          <fg-input  type="text"
-                     name="logradouro"
-                     v-validate="modelValidations.logradouro"
-                     :error="getError('logradouro')"
-                     v-model="model.logradouro">
-          </fg-input>
-          <label>Número</label>
-          <fg-input  type="text"
-                     name="numero"
-                     v-validate="modelValidations.numero"
-                     :error="getError('numero')"
-                     v-model="model.numero">
-          </fg-input>
-          <label>Bairro</label>
-          <fg-input  type="text"
-                     name="bairro"
-                     v-validate="modelValidations.bairro"
-                     :error="getError('bairro')"
-                     v-model="model.bairro">
-          </fg-input>
-          <label>Cidade</label>
-          <fg-input  type="text"
-                     name="cidade"
-                     v-validate="modelValidations.cidade"
-                     :error="getError('cidade')"
-                     v-model="model.cidade">
-          </fg-input>
-          <label>Estado</label>
-          <fg-input  type="text"
-                     name="estado"
-                     v-validate="modelValidations.estado"
-                     :error="getError('estado')"
-                     v-model="model.estado">
-          </fg-input>
+        <div class="card-body">
+            <div class="form-group">
+                <label>Nome</label>
+                <fg-input type="text" name="nome" v-validate="modelValidations.nome" :error="getError('nome')" v-model="model.nome">
+                </fg-input>
+                <label>CNPJ</label>
+                <fg-input type="text" name="cnpj" v-validate="modelValidations.cnpj" :error="getError('cnpj')" v-model="model.cnpj">
+                </fg-input>
+                <label>Telefone</label>
+                <fg-input type="text" name="telefone" v-validate="modelValidations.telefone" :error="getError('telefone')" v-model="model.telefone">
+                </fg-input>
+                <label>E-mail</label>
+                <fg-input type="email" name="email" v-validate="modelValidations.email" :error="getError('email')" v-model="model.email">
+                </fg-input>
+            </div>
+            <div class="form-group">
+                <label>CEP</label>
+                <fg-input type="text" name="cep" v-validate="modelValidations.cep" :error="getError('cep')" @change="buscarEndereco($event)" v-model="model.cep">
+                </fg-input>
+                <label>Logradouro</label>
+                <fg-input type="text" name="logradouro" v-validate="modelValidations.logradouro" :error="getError('logradouro')" v-model="model.logradouro">
+                </fg-input>
+                <label>NÃºmero</label>
+                <fg-input type="text" name="numero" v-validate="modelValidations.numero" :error="getError('numero')" v-model="model.numero">
+                </fg-input>
+                <label>Bairro</label>
+                <fg-input type="text" name="bairro" v-validate="modelValidations.bairro" :error="getError('bairro')" v-model="model.bairro">
+                </fg-input>
+                <label>Cidade</label>
+                <fg-input type="text" name="cidade" v-validate="modelValidations.cidade" :error="getError('cidade')" v-model="model.cidade">
+                </fg-input>
+                <div class="col-lg-6">
+                    <label>Estado</label>
+                    <fg-input :error="getError('estado')">
+                        <el-select class="select-default" v-model="model.estado" name="estado" v-validate="modelValidations.estado" placeholder="Selecione...">
+                            <el-option class="select-default" v-for="item in optionsStade" :key="item.value" :label="item.label" :value="item.value">
+                            </el-option>
+                        </el-select>
+                    </fg-input>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="card-footer text-right">
-        <p-button type="info" @click.prevent="validate">Salvar</p-button>
-      </div>
+        <div class="card-footer text-right">
+            <p-button type="info" @click.prevent="validate">Salvar</p-button>
+        </div>
     </form>
 
-  </div>
+</div>
 </template>
+
 <script>
-  export default {
-    data () {
-      return {
-        model: {
-          nome: '',
-          razao_social: '',
-          cnpj: '',
-          telefone: '',
-          email: '',
-
-          logradouro: '',
-          numero: '',
-          bairro: '',
-          cidade: '',
-          estado: ''
-        },
-        modelValidations: {
-          nome: {
-            required: true
-          },
-          razao_social: {
-            required: true
-          },
-          cnpj: {
-            required: true
-          },
-          telefone: {
-            required: true
-          },
-          email: {
-            required: true,
-            email: true
-          },
-          logradouro: {
-            required: true
-          },
-          numero: {
-            required: true
-          },
-          bairro: {
-            required: true
-          },
-          cidade: {
-            required: true
-          },
-          estado: {
-            required: true
-          },
-
+import axios from 'axios'
+import swal from 'sweetalert2'
+export default {
+    data() {
+        return {
+            model: {
+                nome: '',
+                cnpj: '',
+                telefone: '',
+                email: '',
+                cep: '',
+                logradouro: '',
+                numero: '',
+                bairro: '',
+                cidade: '',
+                estado: ''
+            },
+            modelValidations: {
+                nome: {
+                    required: true
+                },
+                razao_social: {
+                    required: true
+                },
+                cnpj: {
+                    required: true
+                },
+                telefone: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                cep: {
+                    required: true
+                },
+                logradouro: {
+                    required: true
+                },
+                numero: {
+                    required: true
+                },
+                bairro: {
+                    required: true
+                },
+                cidade: {
+                    required: true
+                },
+                estado: {
+                    required: true
+                }
+            },
+            optionsStade: [{
+                    value: 'AC',
+                    label: 'Acre'
+                },
+                {
+                    value: 'AL',
+                    label: 'Alagoas'
+                },
+                {
+                    value: 'AP',
+                    label: 'AmapÃ¡'
+                },
+                {
+                    value: 'AM',
+                    label: 'Amazonas'
+                },
+                {
+                    value: 'BA',
+                    label: 'Bahia'
+                },
+                {
+                    value: 'CE',
+                    label: 'CearÃ¡'
+                },
+                {
+                    value: 'DF',
+                    label: 'Distrito Federal'
+                },
+                {
+                    value: 'ES',
+                    label: 'EspÃ­rito Santo'
+                },
+                {
+                    value: 'GO',
+                    label: 'GoiÃ¡s'
+                },
+                {
+                    value: 'MA',
+                    label: 'MaranhÃ£o'
+                },
+                {
+                    value: 'MT',
+                    label: 'Mato Grosso'
+                },
+                {
+                    value: 'MS',
+                    label: 'Mato Grosso do Sul'
+                },
+                {
+                    value: 'MG',
+                    label: 'Minas Gerais'
+                },
+                {
+                    value: 'PA',
+                    label: 'ParÃ¡na'
+                },
+                {
+                    value: 'PB',
+                    label: 'ParaÃ­ba'
+                },
+                {
+                    value: 'PR',
+                    label: 'ParanÃ¡'
+                },
+                {
+                    value: 'PE',
+                    label: 'Pernambuco'
+                },
+                {
+                    value: 'PI',
+                    label: 'PiauÃ­'
+                },
+                {
+                    value: 'RJ',
+                    label: 'Rio de Janeiro'
+                },
+                {
+                    value: 'RN',
+                    label: 'Rio Grande do Norte'
+                },
+                {
+                    value: 'RS',
+                    label: 'Rio Grande do Sul'
+                },
+                {
+                    value: 'RO',
+                    label: 'RondÃ´nia'
+                },
+                {
+                    value: 'RR',
+                    label: 'Roraima'
+                },
+                {
+                    value: 'SC',
+                    label: 'Santa Catarina'
+                },
+                {
+                    value: 'SP',
+                    label: 'SÃ£o Paulo'
+                },
+                {
+                    value: 'SE',
+                    label: 'Sergipe'
+                },
+                {
+                    value: 'TO',
+                    label: 'Tocantins'
+                }
+            ]
         }
-      }
     },
     methods: {
-      getError (fieldName) {
-        return this.errors.first(fieldName)
-      },
-      validate () {
-        this.$validator.validateAll().then(isValid => {
-          this.$emit('on-submit', this.registerForm, isValid)
-        })
-      }
-    }
-  }
+        getError(fieldName) {
+            return this.errors.first(fieldName)
+        },
+        validate() {
+            this.$validator.validateAll().then(isValid => {
+                this.$emit('on-submit', this.salvar(), isValid)
+            })
+        },
+        buscarEndereco() {
+            this.model.bairro = ''
+            this.model.logradouro = ''
+            this.model.cidade = ''
+            this.model.estado = ''
+
+            axios.get('https://api.postmon.com.br/v1/cep/' + this.model.cep)
+                .then(response => {
+                    this.endereco = response.data
+                    if (this.endereco.cidade) {
+                        this.model.cidade = this.endereco.cidade
+                    }
+                    if (this.endereco.bairro) {
+                        this.model.bairro = this.endereco.bairro
+                    }
+                    if (this.endereco.logradouro) {
+                        this.model.logradouro = this.endereco.logradouro
+                    }
+                    if (this.endereco.complemento) {
+                        this.model.complemento = this.endereco.complemento
+                    }
+                    if (this.endereco.estado) {
+                        this.model.estado = this.endereco.estado
+                    }
+                })
+                .catch(error => {
+                    // alert('Erro no cadastro do EndereÃÂ§o')
+                    console.log(error.response.data)
+                })
+        },
+        salvar() {
+
+            let empresa = {
+                nome: this.model.nome,
+                razao_social: this.model.nome,
+                telefone: this.model.telefone,
+                email: this.model.email,
+                cnpj: this.model.cnpj
+            }
+            let endereco = {
+                logradouro: this.model.nome,
+                cep: this.model.logradouro,
+                uf: this.model.uf,
+                bairro: this.model.bairro,
+                cidade: this.model.cidade,
+                numero: this.model.numero,
+                tipo: 'Comercial'
+            }
+            axios.post(process.env.VUE_APP_ROOT_API + '/escritorio', empresa)
+                .then(response => {
+                    this.results = response.data
+                    endereco.id_escritorio = response.data.id
+                    // Cadastro de Endereço
+                    axios.post(process.env.VUE_APP_ROOT_API + '/endereco', endereco)
+                        .then(response => {
+                            this.resultAdress = response.data
+                            swal('Bom trabalho!', 'Escritório Cadastrado com sucesso!', 'success')
+                            this.$router.push('/forms/OfficeList')
+                        })
+                        .catch(error => {
+                            swal('Algo de errado!', 'Verifique os campos do cadastro!', 'error')
+                            console.log(error.response.data)
+                        })
+                })
+                .catch(error => {
+                    swal('Algo de errado!', 'Verifique os campos do cadastro!', 'error')
+                    console.log(error.response.data)
+                })
+        }
+    },
+
+}
 </script>
+
 <style>
 </style>
--- src/components/Dashboard/Views/Lists/ListCompany.vue
+++ src/components/Dashboard/Views/Lists/ListCompany.vue
@@ -1,194 +1,139 @@
 <template>
-  <div>
+<div>
     <div class="card">
-      <div class="card-header">
-        <div class="row">
-          <!-- ------------------------ -->
-          <div class="col-sm-10">
-            <div class="card-body text-left">
-                <div><h5 class="card-title">Empresas</h5></div>
+        <div class="card-header">
+            <div class="row">
+                <!-- ------------------------ -->
+                <div class="col-sm-10">
+                    <div class="card-body text-left">
+                        <div>
+                            <h5 class="card-title">Empresas</h5>
+                        </div>
+                    </div>
+                </div>
+                <!-- ------------------------- -->
+                <div>
+                    <div class="col-sm-1">
+                        <div class="iten-center">
+                            <p-button type="primary" @click="handleLike()">Cadastro</p-button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <!-- ------------------------- -->
-          <div>
-            <div class="col-sm-1">
-              <div class="iten-center">
-                <p-button type="primary"  @click="handleLike()">Cadastro</p-button>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-      <div class="card-body row">
-        <div class="col-sm-12">
-          <el-table :data="tableData" header-row-class-name="text-primary">
-            <el-table-column type="index">
+        <div class="card-body row">
+            <div class="col-sm-12">
+                <el-table :data="tableData" header-row-class-name="text-primary">
+                    <el-table-column type="index">
 
-            </el-table-column>
-            <el-table-column prop="nome"
-                             label="Nome">
-            </el-table-column>
-            <el-table-column prop="job"
-                             label="Razão Social">
-            </el-table-column>
-            <el-table-column prop="salary"
-                             label="CNPJ">
-            </el-table-column>
-            <el-table-column prop="salary"
-                             label="Telefone">
-            </el-table-column>
-            <el-table-column prop="salary"
-                             label="E-mail">
-            </el-table-column>
-            <el-table-column
-              class-name="action-buttons td-actions"
-              align="right"
-              label="Ações">
-              <template slot-scope="props">
-                <p-button type="success" size="sm" icon @click="handleEdit(props.$index, props.row)">
-                  <i class="fa fa-edit"></i>
-                </p-button>
-                <p-button type="danger" size="sm" icon @click="handleDelete(props.$index, props.row)">
-                  <i class="fa fa-times"></i>
-                </p-button>
-              </template>
-            </el-table-column>
-          </el-table>
+                    </el-table-column>
+                    <el-table-column prop="nome" label="Nome">
+                    </el-table-column>
+                    <el-table-column prop="razao_social" label="Razão Social">
+                    </el-table-column>
+                    <el-table-column prop="cnpj" label="CNPJ">
+                    </el-table-column>
+                    <el-table-column prop="telefone" label="Telefone">
+                    </el-table-column>
+                    <el-table-column prop="email" label="E-mail">
+                    </el-table-column>
+                    <el-table-column class-name="action-buttons td-actions" align="right" label="Ações">
+                        <template slot-scope="props">
+                            <p-button type="success" size="sm" icon @click="handleEdit(props.$index, props.row)">
+                                <i class="fa fa-edit"></i>
+                            </p-button>
+                            <p-button type="danger" size="sm" icon @click="handleDelete(props.$index, props.row)">
+                                <i class="fa fa-times"></i>
+                            </p-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 </template>
+
 <script>
-  import swal from 'sweetalert2'
-  import axios from 'axios'
-  import Vue from 'vue'
-  import {Table, TableColumn} from 'element-ui'
-  import PSwitch from 'src/components/UIComponents/Switch.vue'
-  Vue.use(Table)
-  Vue.use(TableColumn)
-  export default{
+import swal from 'sweetalert2'
+import axios from 'axios'
+import Vue from 'vue'
+import {
+    Table,
+    TableColumn
+} from 'element-ui'
+import PSwitch from 'src/components/UIComponents/Switch.vue'
+Vue.use(Table)
+Vue.use(TableColumn)
+export default {
     components: {
-      PSwitch
+        PSwitch
     },
-    data () {
-      return {
-        tableData: [
-        /*
-        {
-          name: 'Andrew Mike',
-          job: 'Develop',
-          salary: '€ 99,225',
-          active: false
-        }, {
-          name: 'John Doe',
-          job: 'Design',
-          salary: '€ 89,241',
-          active: false
-        }, {
-          name: 'Alex Mike',
-          job: 'Design',
-          salary: '€ 92,144',
-          active: false
-        }, {
-          name: 'Mike Monday',
-          job: 'Marketing',
-          salary: '€ 49,990',
-          active: true
-        },
-        {
-          name: 'Paul dickens',
-          job: 'Communication',
-          salary: '€ 69,201',
-          active: true
+    data() {
+        return {
+            tableData: [],
+            productsTable: []
         }
-        */
-       ],
-        productsTable: [
-
-          {
-            image: 'static/img/tables/agenda.png',
-            title: 'Notebook',
-            subTitle: 'Most beautiful agenda for the office.',
-            price: 49,
-            quantity: 1
-          },
-          {
-            image: 'static/img/tables/stylus.jpg',
-            title: 'Stylus',
-            subTitle: 'Design is not just what it looks like and feels like. Design is how it works.',
-            price: 499,
-            quantity: 2
-          },
-          {
-            image: 'static/img/tables/evernote.png',
-            title: 'Evernote iPad Stander',
-            subTitle: 'A groundbreaking Retina display. All-flash architecture. Fourth-generation Intel processors.',
-            price: 799,
-            quantity: 1
-          }
-
-        ]
-      }
     },
-    mounted () {
-       axios.get(process.env.VUE_APP_ROOT_API +'/empresa').then(response => {
-        this.tableData = response.data
-      })
+    mounted() {
+        axios.get(process.env.VUE_APP_ROOT_API + '/empresa?where={"ativo": 1}').then(response => {
+            this.tableData = response.data
+        })
     },
     methods: {
-      handleLike () {
-        this.$router.push('/forms/company')
-      },
-      handleEdit (index, row) {
-        alert(`Your want to edit ${row.name}`)
-      },
-      handleDelete (index, row) {
-        let empresa = {
+        handleLike() {
+            this.$router.push('/forms/company')
+        },
+        handleEdit(index, row) {
+            alert(`Your want to edit ${row.name}`)
+        },
+        handleDelete(index, row) {
+            let empresa = {
                 ativo: false
-                /*razao_social: this.model.nome,
-                site: this.form.site,
-                telefone: this.form.telefone,
-                email: this.form.email,
-                cnpj: this.form.cnpj*/
-                }
-                axios.put(process.env.VUE_APP_ROOT_API  + '/empresa/'+row.id, empresa)
+            }
+            axios.put(process.env.VUE_APP_ROOT_API + '/empresa/' + row.id, empresa)
                 .then(response => {
                     this.results = response.data
-                    // alert(`Empresa deletada com sucesso ${row.nome}`)
-                    swal('Bom trabalho!', `Empresa ${row.nome} deletada com sucesso!`, 'success')
-                    window.location.reload()
+                    axios.get(process.env.VUE_APP_ROOT_API + '/empresa?where={"ativo": 1}').then(response => {
+                        this.tableData = response.data
+                        swal('Bom trabalho!', `Empresa ${row.nome} deletada com sucesso!`, 'success')
                     })
-                    .catch(error => {
-                        alert(error.response)
-                        console.log(error.response.data)
-                        })
-      },
-      getSummaries (param) {
-        const { columns } = param
-        const sums = []
-        columns.forEach((column, index) => {
-          if (index === 0) {
-            sums[index] = 'Total'
-          } else if (index < columns.length - 1) {
-            sums[index] = ''
-          } else {
-            let sum = 0
-            this.productsTable.forEach((obj) => {
-              sum += obj.quantity * obj.price
+                    //this.$router.push('/forms/companyList')
+                })
+                .catch(error => {
+                    alert(error.response)
+                    console.log(error.response.data)
+                })
+
+        },
+        getSummaries(param) {
+            const {
+                columns
+            } = param
+            const sums = []
+            columns.forEach((column, index) => {
+                if (index === 0) {
+                    sums[index] = 'Total'
+                } else if (index < columns.length - 1) {
+                    sums[index] = ''
+                } else {
+                    let sum = 0
+                    this.productsTable.forEach((obj) => {
+                        sum += obj.quantity * obj.price
+                    })
+                    sums[index] = `€ ${sum}`
+                }
             })
-            sums[index] = `€ ${sum}`
-          }
-        })
-        return sums
-      }
+            return sums
+        }
     }
-  }
+}
 </script>
+
 <style lang="scss">
-  .el-table .td-actions{
+.el-table .td-actions {
     button.btn {
-      margin-right: 5px;
+        margin-right: 5px;
     }
-  }
+}
 </style>
