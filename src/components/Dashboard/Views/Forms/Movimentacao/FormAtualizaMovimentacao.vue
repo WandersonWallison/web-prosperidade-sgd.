7bce207b5464d22b584133b5f24b82234cf9cc51
Wellington Santos <wellington.m.santos@gmail.com>
2019-07-03 18:16:27 -0300

    paginação do atualizaçaõ movimentação

--- src/components/Dashboard/Views/Forms/Movimentacao/FormAtualizaMovimentacao.vue
+++ src/components/Dashboard/Views/Forms/Movimentacao/FormAtualizaMovimentacao.vue
@@ -17,7 +17,7 @@
             <div class="col-lg-6">
                 <label>status</label>
                 <fg-input>
-                    <el-select no-data-text="Sem Informações" class="select-default" v-model="model.status" name="status" placeholder="Selecione uma Status.">
+                    <el-select no-data-text="Sem Informações" class="select-default" v-model="model.status" name="status" placeholder="Selecione um status.">
                         <el-option class="select-default" v-for="item in status_movimentacao" :key="item.id" :label="item.descricao" :value="item.id">
                         </el-option>
                     </el-select>
@@ -48,7 +48,23 @@
                 </el-table-column>
             </el-table>
         </div>
+        <div class="col-sm-6 pagination-info">
+            <p class="category">Mostrando {{from + 1}} de {{to}} de {{total}} Entradas</p>
+        </div>
+        <div class="col-sm-6">
+            <p-pagination class="pull-right" v-model="pagination.currentPage" :per-page="pagination.perPage" :total="pagination.total">
+            </p-pagination>
+        </div>
     </div>
+    <div class="text-xs-center">
+        <el-pagination background layout="prev, pager, next" :total=this.qtd>
+        </el-pagination>
+    </div>
+    <md-dialog :md-active.sync="showUpdate">
+        <div>
+            <MovementEdit :selected="selected"></MovementEdit>
+        </div>
+    </md-dialog>
 </div>
 </template>
 
@@ -65,18 +81,70 @@ import swal from 'sweetalert2'
 import {
     Money
 } from 'v-money'
+import PPagination from 'src/components/UIComponents/Pagination.vue'
 import moment from 'moment'
 export default {
     name: 'FormAtualizaMovimentacao2',
+    computed: {
+        pagedData() {
+            return this.tableData.slice(this.from, this.to)
+        },
+        /***
+         * Searches through table data and returns a paginated array.
+         * Note that this should not be used for table with a lot of data as it might be slow!
+         * Do the search and the pagination on the server and display the data retrieved from server instead.
+         * @returns {computed.pagedData}
+         */
+        queriedData() {
+            if (!this.searchQuery) {
+                this.pagination.total = this.tableData.length
+                return this.pagedData
+            }
+            let result = this.tableData
+                .filter((row) => {
+                    let isIncluded = false
+                    for (let key of this.propsToSearch) {
+                        let rowValue = row[key].toString()
+                        if (rowValue.includes && rowValue.includes(this.searchQuery)) {
+                            isIncluded = true
+                        }
+                    }
+                    return isIncluded
+                })
+            this.pagination.total = result.length
+            return result.slice(this.from, this.to)
+        },
+        to() {
+            let highBound = this.from + this.pagination.perPage
+            if (this.total < highBound) {
+                highBound = this.total
+            }
+            return highBound
+        },
+        from() {
+            return this.pagination.perPage * (this.pagination.currentPage - 1)
+        },
+        total() {
+            this.pagination.total = this.tableData.length
+            return this.tableData.length
+        }
+    },
     data() {
         return {
+             pagination: {
+                perPage: 10,
+                currentPage: 1,
+                perPageOptions: [10, 25, 50, 75],
+                total: 0
+            },
             tableData: [],
             status_movimentacao: [],
             multipleSelection: [],
             results: [],
             model: {
                 status: ''
-            }
+            },
+            qtd: null
         }
     },
     filters: {
@@ -103,6 +171,7 @@ export default {
         })
         axios.get(process.env.VUE_APP_ROOT_API + '/movimentacao?where={"ativo": 1}').then(response => {
             this.tableData = response.data
+            this.qtd = this.tableData.length
         })
     },
     methods: {
@@ -149,3 +218,10 @@ export default {
     }
 }
 </script>
+<style lang="scss">
+.el-table .td-actions {
+    button.btn {
+        margin-right: 5px;
+    }
+}
+</style>
