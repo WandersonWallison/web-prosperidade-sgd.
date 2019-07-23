161791f14b6c6a007119005e33a574b63942f36c
Wellington Santos <wellington.m.santos@gmail.com>
2019-07-23 11:42:38 -0300

    legibilidade de codigo

--- src/progressbar.js
+++ src/progressbar.js
@@ -19,29 +19,25 @@ export default function initProgress(router) {
 
   router.beforeEach((to, from, next) => {
 
-    const links = JSON.parse(window.localStorage.getItem('links'));
-
+    const links = JSON.parse(window.localStorage.getItem('links'))
     // regra de validação de users
     if (to.path === '/login' || to.path === '/admin/overview') {
       return next()
     } else {
-        //console.log(links)
       if (links){
         for (let index = 0; index < links.length; index++) {
-          if (to.path === links[index].link){
+          if (to.path === links[index].link) {
             ValidaLink = true
             tryInitProgress()
             return next()
-         }
+          }
         }
       }
       if(ValidaLink){
          swal('Você não tem permissão', '','warning')
       }
-
     }
   })
-
   router.afterEach(() => {
     // Complete the animation of the route progress bar.
     routeResolved = true;
