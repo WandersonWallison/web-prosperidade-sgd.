a02dc33b86ec9ce197ea0bff121cda4ada144694
Wellington Santos <wellington.m.santos@gmail.com>
2019-08-09 04:19:09 -0300

    atualização em massa

--- src/components/Dashboard/Views/Forms/Movimentacao/FormAtualizaMovimentacao.vue
+++ src/components/Dashboard/Views/Forms/Movimentacao/FormAtualizaMovimentacao.vue
@@ -2,62 +2,61 @@
 <div class="row">
     <div class="col-md-12 card">
         <div class="card-body row">
-            <div class="col-sm-9">
-                <div class="card-body text-left">
-                    <div>
-                        <h5 class="card-title">Atualizações de Movimentações</h5>
-                    </div>
+
+            <div class="card-body text-left">
+                <h5 class="card-title">Atualizações de Movimentações</h5>
+                <p-button type="primary" @click="salvar()">Atualizar Movimentações</p-button>
+            </div>
+            <div class="card">
+                <div slot="header">
+                    PESQUISA
                 </div>
-            </div>
-            <div class="col-sm-3">
-                <div class="pull-right">
-                    <p-button type="primary" @click="salvar()">Atualizar Movimentações</p-button>
+                <div>
+                    <label>Status</label>
+                    <fg-input>
+                        <el-select no-data-text="Carregando..." class="select-default" v-model="model.status" name="status" placeholder="Selecione status">
+                            <el-option class="select-default" value="">Selecione</el-option>
+                            <el-option class="select-default" v-for="item in status_movimentacao" :key="item.id" :label="item.descricao" :value="item.id">
+                            </el-option>
+                        </el-select>
+                    </fg-input>
                 </div>
+                <div>
+                    <label>Data Registro</label>
+                    <fg-input>
+                        <el-date-picker v-model="value1" type="daterange" format="dd/MM/yyyy" range-separator="|" start-placeholder="Data Inicial" end-placeholder="Data Final">
+                        </el-date-picker>
+                    </fg-input>
+                </div>
+                <p-button type="primary" @click="buscarMovimentacao()">Buscar</p-button>
             </div>
-            <div class="col-lg-4">
-                <label>Status</label>
-                <fg-input>
-                    <el-select no-data-text="Carregando..." class="select-default" v-model="model.status" name="status" @change="filtrarStatus()" placeholder="Selecione um status.">
-                        <el-option class="select-default" v-for="item in status_movimentacao" :key="item.id" :label="item.descricao" :value="item.id">
-                        </el-option>
-                    </el-select>
-                </fg-input>
-            </div>
-            <div class="col-lg-4">
-               <label>Data Registro</label>
-                <el-date-picker
-                    v-model="value1"
-                    type="daterange"
-                    format="dd/MM/yyyy"
-                    range-separator="|"
-                    start-placeholder="Data Inicial"
-                    end-placeholder="Data Final">
-                </el-date-picker>
-            </div>
+            <br>
+
         </div>
         <div class="col-sm-12 mt-2">
-            <el-table ref="multipleTable" empty-text="Carregando..." :data="tableData" style="width: 100%" @selection-change="handleSelectionChange">
-                <el-table-column   type="selection" width="50">
+            <el-table ref="multipleTable" empty-text="Sem informações" :data="tableData" style="width: 100%" @selection-change="handleSelectionChange">
+                <el-table-column type="selection" width="50">
                 </el-table-column>
-                <el-table-column  align="center" label="CÓDIGO" width="88">
+                <el-table-column align="center" label="CÓDIGO" width="88">
                     <template slot-scope="scope">{{ scope.row.id }}</template>
                 </el-table-column>
-                <el-table-column  label="CLIENTE" width="250">
+                <el-table-column label="CLIENTE" width="250">
                     <template slot-scope="scope">{{ scope.row.id_cliente.nome }}</template>
                 </el-table-column>
-                <el-table-column  label="SITUAÇÃO" width="190">
+                <el-table-column label="SITUAÇÃO" width="190">
                     <template slot-scope="scope">{{ scope.row.id_situacao_movimento.descricao }}</template>
                 </el-table-column>
-                <el-table-column  label="VALOR" width="150">
+                <el-table-column label="VALOR" width="150">
                     <template slot-scope="scope">{{ scope.row.valor | formatarMoeda }}</template>
                 </el-table-column>
-                <el-table-column  align="center" label="DATA REGISTRO" width="150">
-                    <template slot-scope="scope">{{ scope.row.dthr_registro | maskData }}</template>
+                <el-table-column align="center" label="DATA REGISTRO" width="150">
+                    <template slot-scope="scope">{{ scope.row.data_registro | maskData }}</template>
                 </el-table-column>
-                <el-table-column  align="left" label="OBSERVAÇÃO" width="200">
+                <el-table-column align="left" label="OBSERVAÇÃO" width="200">
                     <template slot-scope="scope">{{ scope.row.observacao }}</template>
                 </el-table-column>
             </el-table>
+            <!--
             <div class="col-sm-12 pagination-info">
                 <p class="category">Mostrando {{from + 1}} de {{to}} de {{total}} Entradas</p>
             </div>
@@ -65,6 +64,7 @@
                 <p-pagination class="pull-right" v-model="pagination.currentPage" :per-page="pagination.perPage" :total="pagination.total">
                 </p-pagination>
             </div>
+            -->
         </div>
     </div>
     <md-dialog :md-active.sync="showUpdate">
@@ -80,6 +80,7 @@ import Vue from 'vue'
 import {
     Table,
     TableColumn,
+    DatePicker,
     Select,
     Option
 } from 'element-ui'
@@ -92,16 +93,21 @@ import PPagination from 'src/components/UIComponents/Pagination.vue'
 import moment from 'moment'
 import MovementEdit from './FormMovementEdit'
 export default {
-    name: 'FormAtualizaMovimentacao2',
+    name: 'FormAtualizaMovimentacao',
+
     props: {
         selected: {
+            type: Object
+        },
+        value: {
             type: Object
         }
     },
     components: {
         PPagination,
         MovementEdit,
-        Money
+        Money,
+        [DatePicker.name]: DatePicker,
     },
     computed: {
         pagedData() {
@@ -165,35 +171,34 @@ export default {
             },
             qtd: null,
             pickerOptions: {
-            shortcuts: [
-                {
-                    text: 'Last week',
-                    onClick(picker) {
-                        const end = new Date();
-                        const start = new Date();
-                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
-                        picker.$emit('pick', [start, end]);
+                shortcuts: [{
+                        text: 'Last week',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: 'Last month',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                            picker.$emit('pick', [start, end]);
+                        }
+                    },
+                    {
+                        text: 'Last 3 months',
+                        onClick(picker) {
+                            const end = new Date();
+                            const start = new Date();
+                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                            picker.$emit('pick', [start, end]);
+                        }
                     }
-                }, 
-                {
-                    text: 'Last month',
-                    onClick(picker) {
-                        const end = new Date();
-                        const start = new Date();
-                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
-                        picker.$emit('pick', [start, end]);
-                    }
-                }, 
-                {
-                    text: 'Last 3 months',
-                    onClick(picker) {
-                        const end = new Date();
-                        const start = new Date();
-                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
-                        picker.$emit('pick', [start, end]);
-                    }
-                }
-            ]
+                ]
             },
             value1: '',
             value2: ''
@@ -213,11 +218,11 @@ export default {
         }
     },
     mounted() {
-        
-        axios.get(process.env.VUE_APP_ROOT_API + '/tipo_situacao_movimento?where={"ativo": 1}').then(response => {
+
+        axios.get(process.env.VUE_APP_ROOT_API + '/tipo_situacao_movimento?where={"ativo": 1}&limit=1000').then(response => {
             this.status_movimentacao = response.data
         })
-        axios.get(process.env.VUE_APP_ROOT_API + '/movimentacao?where={"ativo": 1,"id_tipo_movimentacao":2}').then(response => {
+        axios.get(process.env.VUE_APP_ROOT_API + '/movimentacao?where={"ativo": 1,"id_tipo_movimentacao":2}&limit=10000000').then(response => {
             this.tableData = response.data
             this.qtd = this.tableData.length
         })
@@ -263,10 +268,48 @@ export default {
                 this.$router.push('/forms/MovementList')
             })
         },
-        filtrarStatus() {
-            axios.get(process.env.VUE_APP_ROOT_API + '/movimentacao?id_situacao_movimento='+this.model.status).then(response => {
-                this.tableData = response.data                
+        /*
+        buscarMovimentacao() {
+            if (this.model.status) {
+                axios.get(process.env.VUE_APP_ROOT_API + '/movimentacao?where={"ativo":1,"id_situacao_movimento":' + this.model.status + '}').then(response => {
+                    this.tableData = response.data
+                })
+            } else {
+                axios.get(process.env.VUE_APP_ROOT_API + '/movimentacao?where={"ativo":1}').then(response => {
+                    this.tableData = response.data
+                })
+            }
+        },
+        */
+        buscarMovimentacao() {
+
+            console.log('Data 1', this.maskDataFiltro(this.value1[0]))
+            console.log('Data 2', this.maskDataFiltro(this.value1[1]))
+
+            let filtro = '?where={"ativo":1,"id_tipo_movimentacao":2}&limit=10000000'
+
+            if (this.model.status && this.value1.length != 0) {
+                filtro = '?where={"data_registro":{">=": "' + this.maskDataFiltro(this.value1[0]) + '","<=":"' + this.maskDataFiltro(this.value1[1]) + '"},"id_situacao_movimento":' + this.model.status + ',"id_tipo_movimentacao":2,"ativo":1}&limit=10000000'
+                // alert('chegou o status e as datas')
+            } else if (this.model.status) {
+                filtro = '?where={"id_situacao_movimento":' + this.model.status + ',"id_tipo_movimentacao":2,"ativo":1}&limit=10000000'
+                // alert('chegou apenas o status')
+            } else if (this.value1.length != 0) {
+                filtro = '?where={"data_registro":{">=": "' + this.maskDataFiltro(this.value1[0]) + '","<=":"' + this.maskDataFiltro(this.value1[1]) + '"},"id_tipo_movimentacao":2,"ativo":1}&limit=10000000'
+                // alert('chegou as datas')
+            }
+            console.log('Filtro', filtro)
+            this.getMovimentacaoPorFiltro(filtro)
+        },
+        getMovimentacaoPorFiltro(filtro) {
+
+            axios.get(process.env.VUE_APP_ROOT_API + '/movimentacao' + filtro).then(response => {
+                this.tableData = response.data
             })
+        },
+        maskDataFiltro(v) {
+            v = moment(v).format('YYYY-MM-DD')
+            return v
         }
     }
 }
